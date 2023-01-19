ed3cb89c1ade95c9631539917c333993e70ba77c
LaQuannT <117905467+LaQuannT@users.noreply.github.com>
2023-01-19 14:13:56 +0000

    Edited index and script to create a function that displays running scores

--- script.js
+++ script.js
@@ -12,26 +12,6 @@ function getComputersChoice() {
     
 }
 
-// Get players choice - ask for input via prompt
-
-function getPlayersChoice() {
-    let playerChoice = prompt("Rock, Paper, Scissors?");
-    switch(playerChoice) {
-        case "Rock":
-            return "Rock";
-        case "Paper":
-            return "Paper";
-        case "Scissors":
-            return "Scissors";
-
-        // Check if input is valid - input is case sensitive
-
-        default:
-            alert("Please enter a valid choice, choice must Match prompt!");
-            return getPlayersChoice();
-        }
-}
-
 /* Compare players choice to computers choice
 If 'rock' meets 'paper' - 'paper' wins
 If 'rock' meets'scissors' -'rock' wins
@@ -39,34 +19,42 @@ If 'paper' meets'scissors' -'scissors' wins
 If computers choice matches players choice tie */
 
 function playRound(playerSelection, computerSelection){
+    let c = 'computer', p = 'player';
+
     if (playerSelection === "Rock" && computerSelection === "Paper") {
         result.textContent = "You Lose! Paper beats Rock";
         results.append(result);
+        displayScore(c);
         return;
 
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         result.textContent = "You Win! Rock beats Scissors";
         results.append(result);
+        displayScore(p);
         return;
 
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         result.textContent = "You Win! Paper beats Rock";
         results.append(result);
+        displayScore(p);
         return;
 
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         result.textContent = "You Lose! Scissors beats Paper";
         results.append(result);
+        displayScore(c);
         return;
 
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         result.textContent = "You win! Scissors beats Paper";
         results.append(result);
+        displayScore(p);
         return;
 
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
         result.textContent = "You Lose! Rock beats Scissors";
         results.append(result);
+        displayScore(c);
         return;
 
     } else {
@@ -95,10 +83,12 @@ const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const results = document.querySelector('#results');
 const result = document.createElement('p');
+const scores = document.getElementById('scores');
 
 // Add event listeners to rock, paper, and scissors buttons
 rock.addEventListener('click', function() {
     playRound("Rock",getComputersChoice());
+
 });
 
 paper.addEventListener('click', function() {
@@ -109,3 +99,34 @@ scissors.addEventListener('click', function() {
     playRound("Scissors",getComputersChoice());
 });
 
+/* TODO: Create a function that keeps track of scores
+    Display the running scores
+    Announce a winner when a player reaches 5 points */
+
+let playerScore = 0, computerScore = 0;
+
+function displayScore(winner) {
+    if (winner === 'player') {
+        playerScore++;
+        scores.textContent =`Player: ${playerScore} | Computer: ${computerScore}`;
+        return playerScore;
+        
+    } else if (winner === "computer") {
+        computerScore++;
+        scores.textContent =`Player: ${playerScore} | Computer: ${computerScore}`;
+        return computerScore;
+
+    }
+}
+
+/*
+if (playerScore === 5) {
+    result.textContent = "Game Over! Player Wins!";
+    results.append(result);
+
+     } else if (computerScore === 5) {
+        result.textContent = "Game over! Computer Wins!";
+         results.append(result);
+    }
+*/ 
+ 
